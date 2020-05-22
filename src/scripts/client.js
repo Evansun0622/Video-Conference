d088a48e92de85c4d33dce3138da5cfe01c4306d
Evan Sun <46755542+Evansun0622@users.noreply.github.com>
2020-05-22 15:25:53 -0400

    Add files via upload

---
+++ src/scripts/client.js
@@ -0,0 +1,28 @@
+const avengersNames = ['Thor', 'Cap', 'Tony Stark', 'Black Panther', 'Black Widow', 'Hulk', 'Spider-Man'];
+let randomName = avengersNames[Math.floor(Math.random() * avengersNames.length)];
+
+const main = async () => {
+    /* Events handlers */
+    VoxeetSDK.initialize('ZXZmcWVxOGFtbGlzZA==', 'MXRhYm90dmUwaG5hMm5ta2Q1bjdkM2VscjY=');
+
+    VoxeetSDK.conference.on('streamAdded', (participant, stream) => {
+        if (stream.type === 'ScreenShare') return addScreenShareNode(stream);
+        addVideoNode(participant, stream);
+        addParticipantNode(participant);
+    });
+
+    VoxeetSDK.conference.on('streamRemoved', (participant, stream) => {
+        if (stream.type === 'ScreenShare') return removeScreenShareNode();
+        removeVideoNode(participant);
+        removeParticipantNode(participant);
+    });
+
+    try {
+        await VoxeetSDK.session.open({ name: randomName });
+        initUI();
+    } catch (e) {
+        alert('Something went wrong : ' + e);
+    }
+}
+
+main();
